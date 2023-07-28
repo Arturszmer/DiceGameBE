c506f340ba2aa92e6ae18b44a02f91efd1d7ebf5
aszmer@software.altkom.pl <aszmer@software.altkom.pl>
2023-07-28 08:33:50 +0200

    Change add player logic - create player by url with name in request param

--- src/main/java/com/example/DiceGameBE/controller/GamePlayersController.java
+++ src/main/java/com/example/DiceGameBE/controller/GamePlayersController.java
@@ -1,13 +1,12 @@
 package com.example.DiceGameBE.controller;
 
-import com.example.DiceGameBE.dto.CreatePlayerDto;
 import com.example.DiceGameBE.service.GamePlayersProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +17,8 @@ public class GamePlayersController {
     @PostMapping("/{game-id}/add-player")
     public ResponseEntity<String> addPlayer(
             @PathVariable("game-id") String gameId,
-                                            @RequestBody CreatePlayerDto playerDto){
-        boolean isAdded = gamePlayersProvider.addPlayerToOpenGame(playerDto, gameId);
+                                            @RequestParam String playerName){
+        boolean isAdded = gamePlayersProvider.addPlayerToOpenGame(playerName, gameId);
         return isAdded
                 ? ResponseEntity.ok("New Player has been added")
                 : ResponseEntity.ok("Something went wrong, user was not added");
--- src/main/java/com/example/DiceGameBE/service/GamePlayersProviderImpl.java
+++ src/main/java/com/example/DiceGameBE/service/GamePlayersProviderImpl.java
@@ -1,6 +1,5 @@
 package com.example.DiceGameBE.service;
 
-import com.example.DiceGameBE.dto.CreatePlayerDto;
 import com.example.DiceGameBE.model.Game;
 import com.example.DiceGameBE.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
-import static com.example.DiceGameBE.assemblers.PlayerAssembler.toEntity;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,11 +16,11 @@ public class GamePlayersProviderImpl implements GamePlayersProvider {
     private final GameRepository gameRepository;
 
     @Override
-    public boolean addPlayerToOpenGame(CreatePlayerDto playerDto, String gameId) {
+    public boolean addPlayerToOpenGame(String playerName, String gameId) {
         Optional<Game> game = gameRepository.findById(gameId);
         if(game.isPresent()){
-            game.get().addPlayer(toEntity(playerDto));
-            log.info("New Player has been added, his name is: {}", playerDto.getUsername());
+            game.get().addPlayer(playerName);
+            log.info("New Player has been added, his name is: {}", playerName);
             gameRepository.save(game.get());
             return true;
         } else {
