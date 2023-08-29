c4afcc1c3a4c257bd751bb53f36889c722fa8a5a
Tomasz-asus <81529362+Tomasz-asus@users.noreply.github.com>
2023-08-29 09:23:06 +0200

    T-004-dice-model-v6

--- src/main/java/com/example/DiceGameBE/model/Dice.java
+++ src/main/java/com/example/DiceGameBE/model/Dice.java
@@ -11,13 +11,13 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class Dice implements Serializable {
 
-    private Integer Value;
+    private Integer value;
     private boolean isGoodNumber;
     private boolean isChecked;
     private boolean isMultiple;
     private boolean isImmutable;
 
     public Dice(Integer value) {
-        Value = value;
+        this.value = value;
     }
 }
